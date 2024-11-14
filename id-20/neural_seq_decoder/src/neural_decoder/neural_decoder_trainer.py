640431d922cc8467806c65d65ff380d7407ccce0
vickariofillis <vickariofillis@gmail.com>
2024-11-14 17:23:20 -0500

    Small changes

--- id-20/neural_seq_decoder/src/neural_decoder/neural_decoder_trainer.py
+++ id-20/neural_seq_decoder/src/neural_decoder/neural_decoder_trainer.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 
 from .model import GRUDecoder
 from .dataset import SpeechDataset
-from torchinfo import summary
+# from torchinfo import summary
 
 def getDatasetLoaders(
     datasetName,
