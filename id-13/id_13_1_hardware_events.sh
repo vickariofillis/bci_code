816d2fc460fd8287f31473ed9c88791ea23d3d29
vickariofillis <vickariofillis@gmail.com>
2024-11-20 15:38:50 -0500

    First test jobs script

--- id-13/id_13_1_hardware_events.sh
+++ id-13/id_13_1_hardware_events.sh
@@ -4,5 +4,5 @@
 #SBATCH --time=0:15:0
 
 module load anaconda3
-source activate conda_env
-python code.py ...
+# source activate conda_env
+perf stat -o /scratch/e/enright/vickario/research/bci/stats/id-13/1_hardware_events.txt -B -e branch-instructions,branch-misses,cache-misses,cache-references,cpu-cycles,instructions,stalled-cycles-backend,stalled-cycles-frontend python motor.py
