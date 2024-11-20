c1de7c51cef228f8db53de8f0a232e7514c31847
vickariofillis <vickariofillis@gmail.com>
2024-11-20 17:15:04 -0500

    Changed naming convention of output files of the job script

--- id-13/id_13_1_hardware_events.sh
+++ id-13/id_13_1_hardware_events.sh
@@ -2,19 +2,21 @@
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=0:15:0
+#SBATCH -e /scratch/e/enright/vickario/research/bci/stats/id-13/slurm-%x%j.err
+#SBATCH -o /scratch/e/enright/vickario/research/bci/stats/id-13/slurm-%x%j.out
 
 module load anaconda3
 # source activate conda_env
 
 # Start nvidia-smi monitoring in the background and save the output to a file
-nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.used,power.draw --format=csv -l 1 > /scratch/e/enright/vickario/research/bci/stats/id-13/gpu_stats.log &
+nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.used,power.draw --format=csv -l 1 > /scratch/e/enright/vickario/research/bci/stats/id-13/gpu_stats_${SLURM_JOB_ID}.log &
 
 # Get the PID of the nvidia-smi monitoring
 SMI_PID=$!
 
 # perf stat -o /scratch/e/enright/vickario/research/bci/stats/id-13/1_hardware_events.csv -e branch-instructions,branch-misses,cache-misses,cache-references,cpu-cycles,instructions,stalled-cycles-backend,stalled-cycles-frontend -x, python motor.py
 
-perf stat  -e branch-instructions,branch-misses,cache-misses,cache-references,cpu-cycles,instructions,stalled-cycles-backend,stalled-cycles-frontend -x, -o /scratch/e/enright/vickario/research/bci/stats/id-13/1_hardware_events.csv -- python motor.py
+perf stat  -e branch-instructions,branch-misses,cache-misses,cache-references,cpu-cycles,instructions,stalled-cycles-backend,stalled-cycles-frontend -x, -o /scratch/e/enright/vickario/research/bci/stats/id-13/1_hardware_events${SLURM_JOB_ID}.csv -- python motor.py
 
 
 # Kill the nvidia-smi process after the program completes
