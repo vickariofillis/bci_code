5504e0edca498cc7d87451c532437daa0647e7ee
vickariofillis <vickariofillis@gmail.com>
2024-12-04 17:36:25 -0500

    Testing - Regex in bash

--- id-13/scripts/id_13.sh
+++ id-13/scripts/id_13.sh
@@ -10,20 +10,22 @@ declare -a arr=(
                 # "perf_record_1"
                 )
 
+
+
 # Create stats folder of current date if they don't exist
 for i in "${arr[@]}"
 do
-    if [[ ${i} == 'perf_stat_.' ]]; then
+    if [[ ${i} =~ ^perf_stat_. ]]; then
         echo "perf_stat_."
         if [ ! -d "/scratch/e/enright/vickario/research/bci/stats/id-13/perf_stat/$d" ]; then
           mkdir /scratch/e/enright/vickario/research/bci/stats/id-13/perf_stat/$d
         fi
-    elif [[ ${i} == 'perf_record_.' ]]; then
+    elif [[ ${i} =~ ^perf_record_. ]]; then
         echo "perf_record_."
         if [ ! -d "/scratch/e/enright/vickario/research/bci/stats/id-13/perf_record/$d" ]; then
           mkdir /scratch/e/enright/vickario/research/bci/stats/id-13/perf_record/$d
         fi
-    elif [[ ${i} == 'nvidia_smi' ]]; then
+    elif [[ ${i} =~ ^nvidia_smi ]]; then
         echo "nvidia_smi"
         if [ ! -d "/scratch/e/enright/vickario/research/bci/stats/id-13/nvidia_smi/$d" ]; then
           mkdir /scratch/e/enright/vickario/research/bci/stats/id-13/nvidia_smi/$d
