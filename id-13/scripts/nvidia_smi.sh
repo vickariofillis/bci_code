5f7615d203a27812b729283c9981fabd1292e611
vickariofillis <vickariofillis@gmail.com>
2024-12-04 13:52:05 -0500

    Measurement tools output files are moved at the end

--- id-13/scripts/nvidia_smi.sh
+++ id-13/scripts/nvidia_smi.sh
@@ -8,11 +8,8 @@
 
 module load anaconda3
 
-# Get current date
-d=$(date +%Y-%m-%d)
-
 # Start nvidia-smi monitoring in the background and save the output to a file
-nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.used,power.draw --format=csv -l 1 > /scratch/e/enright/vickario/research/bci/stats/id-13/nvidia_smi/${d}/nvidia_smi_${SLURM_JOB_ID}.log &
+nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.used,power.draw --format=csv -l 1 > /scratch/e/enright/vickario/research/bci/stats/temp/nvidia_smi_${SLURM_JOB_ID}.log &
 
 # Get the PID of the nvidia-smi monitoring
 SMI_PID=$!
@@ -20,6 +17,10 @@ SMI_PID=$!
 # Kill the nvidia-smi process after the program completes
 kill $SMI_PID
 
+# Get current date
+d=$(date +%Y-%m-%d)
+
 # Move slurm output files
 mv /scratch/e/enright/vickario/research/bci/stats/temp/slurm_nvidia_smi_${SLURM_JOB_ID}.err /scratch/e/enright/vickario/research/bci/stats/id-13/nvidia_smi/${d}/
 mv /scratch/e/enright/vickario/research/bci/stats/temp/slurm_nvidia_smi_${SLURM_JOB_ID}.out /scratch/e/enright/vickario/research/bci/stats/id-13/nvidia_smi/${d}/
+mv scratch/e/enright/vickario/research/bci/stats/temp/nvidia_smi_${SLURM_JOB_ID}.log /scratch/e/enright/vickario/research/bci/stats/id-13/nvidia_smi/${d}/
