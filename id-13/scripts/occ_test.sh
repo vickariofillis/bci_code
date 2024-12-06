3be819bcf93ff7ddcf24920353c0ed60fdfda771
vickariofillis <vickariofillis@gmail.com>
2024-12-06 17:49:18 -0500

    Testing OCC v3

--- id-13/scripts/occ_test.sh
+++ id-13/scripts/occ_test.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=0:15:0
-#SBATCH -e /scratch/e/enright/vickario/research/bci/stats/temp/slurm_occ__%j.err
+#SBATCH -e /scratch/e/enright/vickario/research/bci/stats/temp/slurm_occ_%j.err
 #SBATCH -o /scratch/e/enright/vickario/research/bci/stats/temp/slurm_occ_%j.out
 
 module load anaconda3
