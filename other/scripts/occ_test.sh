e1f566f2edf21363f00858b6f8da1964ac75ed5e
vickariofillis <vickariofillis@gmail.com>
2024-12-09 18:18:30 -0500

    Reorganized directory structure

---
+++ other/scripts/occ_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=0:15:0
+#SBATCH -e /scratch/e/enright/vickario/research/bci/stats/temp/slurm_occ_%j.err
+#SBATCH -o /scratch/e/enright/vickario/research/bci/stats/temp/slurm_occ_%j.out
+
+module load anaconda3
+
+# Run the CPU burn test and collect OCC metrics
+python3 /scratch/e/enright/vickario/tools/cpu-burn.py 3 /scratch/e/enright/vickario/research/bci/stats/temp/hwmon_metrics.csv
