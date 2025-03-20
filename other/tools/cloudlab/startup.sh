292b9419046b914c06f5cdd133178988d069d1bf
vickariofillis <vickariofillis@gmail.com>
2025-03-20 16:21:16 -0400

    Updated cloudlab scripts

---
+++ other/tools/cloudlab/startup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+################################################################################
+
+### General updates and installing pmu-tools ###
+
+# Update the package lists.
+sudo apt-get update
+
+# Install essential packages: git and build-essential.
+sudo apt-get install -y git build-essential
+
+# Clone the pmu-tools repository.
+git clone https://github.com/andikleen/pmu-tools.git
+cd pmu-tools/
+
+# Install python3-pip and then install the required Python packages.
+sudo apt-get install -y python3-pip
+pip install -r requirements.txt
+
+# Adjust kernel parameters to enable performance measurements.
+sudo sysctl -w 'kernel.perf_event_paranoid=-1'
+sudo sysctl -w 'kernel.nmi_watchdog=0'
+
+# Install perf tools.
+sudo apt-get install -y linux-tools-common linux-tools-generic linux-tools-$(uname -r)
