f8fce78327a357f10c225fae8156d02eb39c81af
vickariofillis <vickariofillis@gmail.com>
2025-03-25 18:55:38 -0400

    Updated ID-13 startup script, ID-20 isn't currently functional

--- other/tools/cloudlab/startup_13.sh
+++ other/tools/cloudlab/startup_13.sh
@@ -6,12 +6,8 @@
 
 # Update the package lists.
 sudo apt-get update
-
 # Install essential packages: git and build-essential.
 sudo apt-get install -y git build-essential
-
-# Install gnome-core
-sudo apt-get install gnome-core
 
 ################################################################################
 
@@ -21,19 +17,15 @@ sudo apt-get install gnome-core
 cd /local
 mkdir tools
 cd tools/
-
 # Clone the pmu-tools repository.
 git clone https://github.com/andikleen/pmu-tools.git
 cd pmu-tools/
-
 # Install python3-pip and then install the required Python packages.
 sudo apt-get install -y python3-pip
 pip install -r requirements.txt
-
 # Adjust kernel parameters to enable performance measurements.
 sudo sysctl -w 'kernel.perf_event_paranoid=-1'
 sudo sysctl -w 'kernel.nmi_watchdog=0'
-
 # Install perf tools.
 sudo apt-get install -y linux-tools-common linux-tools-generic linux-tools-$(uname -r)
 
@@ -49,7 +41,6 @@ cd tools
 curl -L "https://drive.usercontent.google.com/download?id={1BNoA51EHC6VbPVwtkzzw5wSs1pJ2yYD6}&confirm=xxx" -o matlab_R2024b_Linux.zip
 # Unzip Matlab
 unzip matlab_R2024b_Linux.zip -d matlab/
-
 # Download Fieldtrip
 curl -L "https://drive.usercontent.google.com/download?id={1KVb_tsA1KzC7AhaZUKvR0wuR9Ob9bTJe}&confirm=xxx" -o fieldtrip-20240916.zip
 # Unzip Fieldtrip
--- other/tools/cloudlab/startup_20.sh
+++ other/tools/cloudlab/startup_20.sh
@@ -4,35 +4,63 @@
 
 ### General updates
 
+# Add repository for python3.10 version
+# sudo add-apt-repository ppa:deadsnakes/ppa -y
 # Update the package lists.
 sudo apt-get update
-
 # Install essential packages: git and build-essential.
 sudo apt-get install -y git build-essential
+# Install python 3.10
+sudo apt-get install -y python3.10
 
 ################################################################################
 
-# Installing pmu-tools
+### Installing pmu-tools
 
 # Create directories
-cd /local
-mkdir tools
-cd tools/
-
+mkdir /local/tools; cd /local/tools/
 # Clone the pmu-tools repository.
 git clone https://github.com/andikleen/pmu-tools.git
 cd pmu-tools/
-
 # Install python3-pip and then install the required Python packages.
 sudo apt-get install -y python3-pip
 pip install -r requirements.txt
-
 # Adjust kernel parameters to enable performance measurements.
 sudo sysctl -w 'kernel.perf_event_paranoid=-1'
 sudo sysctl -w 'kernel.nmi_watchdog=0'
-
 # Install perf tools.
 sudo apt-get install -y linux-tools-common linux-tools-generic linux-tools-$(uname -r)
+
+################################################################################
+
+### Download other tools related to the workload (ID-20)
+
+# Move to proper directory
+cd /local/tools
+# Clone Kaldi
+git clone https://github.com/kaldi-asr/kaldi.git
+# Clone Pykaldi
+git clone https://github.com/pykaldi/pykaldi.git
+# Download pykaldi
+wget https://github.com/pykaldi/pykaldi/releases/download/v0.2.2/pykaldi-0.2.2-cp310-cp310-linux_x86_64.whl.gz
+# Unzip pykaldi
+gzip -d pykaldi-0.2.2-cp310-cp310-linux_x86_64.whl.gz
+
+################################################################################
+
+### Set up directories for the related tools
+
+# Create directories
+cd /local/tools; mkdir bci_project
+# Transfer necessary files
+cp /local/tools/pykaldi/tools/install_kaldi.sh /local/tools/bci_project
+cp /local/tools/pykaldi/tools/path.sh /local/tools/bci_project
+# Change directory
+cd /local/tools/kaldi/tools/extras
+# Sudo install mkl.sh
+sudo ./install_mkl.sh
+# Move to proper directory
+cd /local/tools
 
 ################################################################################
 
