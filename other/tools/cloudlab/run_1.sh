7b4c6d58b5e2e8d23a5c90f74027c23a0e220b7f
vickariofillis <vickariofillis@gmail.com>
2025-04-26 16:19:55 -0400

    Update run_1.sh
    Added taskset run command version

--- other/tools/cloudlab/run_1.sh
+++ other/tools/cloudlab/run_1.sh
@@ -11,4 +11,14 @@ chown -R $USER /local;
 ### Run workload ID-1 (Seizure Detection - Laelaps)
 
 cd ~;
+
+# Default version
 /local/tools/pmu-tools/toplev -l6 -I 500 --no-multiplex --all -x, -o /local/data/results/c_profile.csv -- /local/code/Laelaps_C/main >> /local/data/results/c_log_file.log 2>&1 | tee /local/data/results/toplev_log.txt
+
+# Taskset version
+# /local/tools/pmu-tools/toplev \
+#   -l6 -I 500 --no-multiplex --all -x, \
+#   -o /local/data/results/c_profile.csv -- \
+#   /usr/bin/taskset -c 0 /local/code/Laelaps_C/main \
+#     >> /local/data/results/c_log_file.log 2>&1 \
+# | tee /local/data/results/toplev_log.txt
