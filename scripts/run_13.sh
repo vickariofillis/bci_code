0c4bb8f16d79f500c202fbc9c46369517b7e04fb
vickariofillis <vickariofillis@gmail.com>
2025-05-12 16:24:27 -0400

    Changes to Cloudlab scripts

--- scripts/run_13.sh
+++ scripts/run_13.sh
@@ -10,3 +10,9 @@ chown -R $USER /local;
 
 ### Run workload ID-13 (Movement Intent)
 
+cd ~;
+
+# Remove processes from Core 8 (CPU 5 and CPU 15) and Core 9 (CPU 6 and CPU 16)
+cset shield --cpu 5,6,15,16 --kthread=on
+
+# Taskset version
--- scripts/startup_20.sh
+++ scripts/startup_20.sh
@@ -95,7 +95,7 @@ sudo apt-get update
 # Install essential packages: git and build-essential.
 sudo apt-get install -y git build-essential
 # Install necessary packages
-sudo apt-get install -y zlib1g-dev automake autoconf cmake sox gfortran libtool protobuf-compiler python3.10 python2.7 pip  python3.10-venv curl g++ graphviz libatlas3-base libtool pkg-config subversion unzip wget
+sudo apt-get install -y zlib1g-dev automake autoconf cmake sox gfortran libtool protobuf-compiler python3.10 python2.7 pip  python3.10-venv curl g++ graphviz libatlas3-base libtool pkg-config subversion unzip wget cpuset
 
 ################################################################################
 
