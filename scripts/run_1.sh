2f72268e719194dd176b8b1ea3394dcacb567b98
vickariofillis <vickariofillis@gmail.com>
2025-05-12 19:18:21 -0400

    Changes to cloudlab scripts

--- scripts/run_1.sh
+++ scripts/run_1.sh
@@ -15,13 +15,11 @@ cd ~;
 # Remove processes from Core 8 (CPU 5 and CPU 15) and Core 9 (CPU 6 and CPU 16)
 cset shield --cpu 5,6,15,16 --kthread=on
 
-# Default version
-/local/tools/pmu-tools/toplev -l6 -I 500 --no-multiplex --all -x, -o /local/data/results/id_1_profile.csv -- /local/code/Laelaps_C/main >> /local/data/results/id_1_profile.log 2>&1 | tee /local/data/results/id_1_toplev.log
-
 # Taskset version
-# /local/tools/pmu-tools/toplev \
-#   -l6 -I 500 --no-multiplex --all -x, \
-#   -o /local/data/results/c_profile.csv -- \
-#   /usr/bin/taskset -c 0 /local/code/Laelaps_C/main \
-#     >> /local/data/results/c_log_file.log 2>&1 \
-# | tee /local/data/results/toplev_log.txt
+sudo cset shield --exec -- sh -c '
+  taskset -c 5 /local/tools/pmu-tools/toplev \
+    -l6 -I 500 --no-multiplex --all -x, \
+    -o /local/data/results/id_1_results.csv -- \
+      taskset -c 6 /local/code/Laelaps_C/main \
+        >> /local/data/results/id_1.log 2>&1 \
+'
