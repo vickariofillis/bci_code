be2cb01cbbaa00a39f9ff343f2cd074258106e26
vickariofillis <vickariofillis@gmail.com>
2025-10-30 00:45:04 -0400

    Fix indentation issues in scripts

--- scripts/startup_20_3gram.sh
+++ scripts/startup_20_3gram.sh
@@ -336,7 +336,7 @@ if [ -f "${PROJECT_DATA}/languageModel.tar.gz" ]; then
 else
     echo "languageModel.tar.gz not found. Downloading..."
     wget --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" \
-         https://datadryad.org/downloads/file_stream/2547356 -O languageModel.tar.gz
+    https://datadryad.org/downloads/file_stream/2547356 -O languageModel.tar.gz
 fi
 # Always extract languageModel.tar.gz
 echo "Extracting languageModel.tar.gz"
